5229a2a5a39ec166980eb1a832655e66a7277981
Dheeraj Singh <dheerajsingh20aug@gmail.com>
2017-05-28 17:45:02 +0530

    Removed loadWeather1

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
-import {createStore, applyMiddleware} from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import reduxThunk from 'redux-thunk'
 
 const DATA_LOADED = "DATA_LOADED"
 
-function reducer(state = {weather: { ready: false } }, action) {
-    switch(action.type) {
+function reducer(state = { weather: { ready: false } }, action) {
+    switch (action.type) {
         case DATA_LOADED:
             return {
                 ready: true,
@@ -27,28 +27,20 @@ function dataReady(data) {
 function loadWeather() {
     return function(dispatch) {
         const url = process.env.REACT_APP_API || "http://api.openweathermap.org/data/2.5/forecast?APPID=019a736fd448ec0464f324f3f7063003&units=metric&q=Newcastle,uk&mode=json"
-        return fetch(url).then(resp => resp.json()).then((data) => dispatch( dataReady(data) ) )
+        return fetch(url).then(resp => resp.json()).then((data) => dispatch(dataReady(data)))
     }
 }
+
 function dataReady1(data) {
-	// Comment added by User 3
+    // Comment added by User 3
     return {
         type: DATA_LOADED,
         data
     }
 }
 
-function loadWeather1() {
-	// Comment added by User 2
-    return function(dispatch) {
-        const url = process.env.REACT_APP_API || "http://api.openweathermap.org/data/2.5/forecast?APPID=019a736fd448ec0464f324f3f7063003&units=metric&q=Newcastle,uk&mode=json"
-        return fetch(url).then(resp => resp.json()).then((data) => dispatch( dataReady(data) ) )
-    }
-}
-
-let store = createStore( reducer, applyMiddleware( reduxThunk) )
+let store = createStore(reducer, applyMiddleware(reduxThunk))
 
 export default store
 
 store.dispatch(loadWeather())
-
